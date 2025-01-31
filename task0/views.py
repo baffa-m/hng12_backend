a379992dfae534637d55a8eede622035ce05e936
Ahmad Almustapha <ahmadalmustapha4@gmail.com>
2025-01-31 14:53:35 +0000

    init

--- task0/views.py
+++ task0/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from datetime import datetime, timezone
+from datetime import datetime
 # Create your views here.
 @api_view()
 def info(request):
     return Response({
         "email": "ahmadalmustapha4@gmail.com",
-        "current_datetime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "current_datetime": datetime.now().isoformat()+"Z",
         "github_url": "https://github.com/baffa-m"
     })
