f927d5df3c295cbe0762f93173697112142c70f0
Ahmad Almustapha <ahmadalmustapha4@gmail.com>
2025-01-31 15:03:26 +0000

    init

--- task0/views.py
+++ task0/views.py
@@ -3,10 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from datetime import datetime
 # Create your views here.
-@api_view()
+@api_view(['GET'])
 def info(request):
     return Response({
         "email": "ahmadalmustapha4@gmail.com",
-        "current_datetime": datetime.now().isoformat()+"Z",
+        "current_datetime": datetime.now().replace(microsecond=0).isoformat()+"Z",
         "github_url": "https://github.com/baffa-m"
     })
